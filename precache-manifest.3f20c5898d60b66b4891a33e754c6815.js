11a39a45b0f944421fad86f86d04064118fc7a93
ToheebAdekunle <toheebspaymentmethod@gmail.com>
2020-08-08 06:40:18 +0100

    Updates

---
+++ precache-manifest.3f20c5898d60b66b4891a33e754c6815.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d700966bb271943937b53174415bfee",
+    "url": "/My-Robot-App/index.html"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/My-Robot-App/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "8726ea72eb088d430862",
+    "url": "/My-Robot-App/static/css/main.7eca9ba2.chunk.css"
+  },
+  {
+    "revision": "01be661733d45e485a8e",
+    "url": "/My-Robot-App/static/js/2.14bc996e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/My-Robot-App/static/js/2.14bc996e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8726ea72eb088d430862",
+    "url": "/My-Robot-App/static/js/main.5b96b204.chunk.js"
+  },
+  {
+    "revision": "b9cb2b5d6ca0edb9b2e7",
+    "url": "/My-Robot-App/static/js/runtime-main.a3d3c58e.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/My-Robot-App/static/media/SEGA.12a3db8b.woff"
+  }
+]);
